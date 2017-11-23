17f82006f6677e60a2cb1f1eaae32a989418c558
Rendy Febry <rendyfebry@gmail.com>
2017-11-23 14:54:44 +0700

    Add command line parameter

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
+
+	var port = flag.String("p", "8080", "Server port")
+	flag.Parse()
+
+	fmt.Println("Server listening on:", *port)
+	log.Fatal(http.ListenAndServe(":"+*port, router))
 }
