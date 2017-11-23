722c01444f559f63a570b2cea257fafce8c29943
Rendy Febry <rendyfebry@gmail.com>
2017-11-23 15:54:06 +0700

    Handle Not Found on single person route

--- cmd/handlers/handlerPersons.go
+++ cmd/handlers/handlerPersons.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -21,7 +22,7 @@ func HandlerPersons(w http.ResponseWriter, r *http.Request) {
 	c := session.DB("test_db").C("persons")
 
 	results := make([]interface{}, 0)
-	errDB = c.Find(bson.M{}).Sort("-name").All(&results)
+	errDB = c.Find(bson.M{}).Select(bson.M{"_id": 0}).Sort("-name").All(&results)
 
 	if errDB != nil {
 		panic(errDB)
@@ -47,15 +48,26 @@ func HandlerPersonsSingle(w http.ResponseWriter, r *http.Request) {
 	c := session.DB("test_db").C("persons")
 
 	var result interface{}
-	errDB = c.Find(bson.M{"email": email}).Sort("-name").One(&result)
+	errDB = c.Find(bson.M{"email": email}).Select(bson.M{"_id": 0}).Sort("-name").One(&result)
 
 	if errDB != nil {
+		if errDB.Error() == "not found" {
+			fmt.Println(errDB.Error())
+
+			w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+			w.WriteHeader(http.StatusNotFound)
+			if err := json.NewEncoder(w).Encode(&models.ResponseObj{Error: 404, Message: "Not Found"}); err != nil {
+				panic(err)
+			}
+			return
+		}
+
 		panic(errDB)
 	}
 
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(&models.ResponseObj{Error: 0, Data: result}); err != nil {
+	if err := json.NewEncoder(w).Encode(&models.ResponseObj{Error: 0, Data: result, Message: "Success"}); err != nil {
 		panic(err)
 	}
 }
